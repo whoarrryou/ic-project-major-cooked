ee378c59f3b68523edcb1abae07261465b769165
whoareyou <krbavadiya11@gmail.com>
2024-10-15 18:58:39 +0530

    One more cumment wow

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    
+
+    printf("we are cooked bro...!@!@!");
+    system("cls");
+    //printf("Aayein baigan");
+    // printf("Line added by yushhh...")
+
+
+    return 0;
+}

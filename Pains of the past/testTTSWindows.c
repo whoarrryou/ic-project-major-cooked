68206eb47c6ec5dfec8036a8969b1869dafcfbbf
whoareyou <krbavadiya11@gmail.com>
2024-11-24 03:11:18 +0530

    fixed the tts.c

--- Pains of the past/testTTSWindows.c
+++ Pains of the past/testTTSWindows.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    const char *text = "Hello, welcome to text to speech in C!";
+    // const char *text = "Hello, welcome to text to speech in C!";
+    char text[100];
+    printf("Enter the text you want to speak: ");
+    fgets(text, 100, stdin);
+    // scanf("%s", text);
+    // fgets(text, 99, stdin);
+    text[strcspn(text, "\n")] = '\0';
     char command[512];
 
     // Format the PowerShell command
